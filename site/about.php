37dd6421c5c1cbd02fce473a5c05106b2a3b6c28
Glen Campbell <glen@broadpool.com>
2013-09-07 11:13:47 -0500

    about pages

---
+++ site/about.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @copyright 2013 Glen Campbell
+ * @license Apache 2.0
+ */
+require 'lib/pictr.php';
+// create a new configuration object
+$CONFIG = new Pictr\Config();
+
+require 'templates/about.html';
